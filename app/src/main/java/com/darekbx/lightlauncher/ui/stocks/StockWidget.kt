3e40421c03a9187b19c5841e586cdae6ad9d317e
Darek <34578369+darekbx@users.noreply.github.com>
2026-03-30 12:53:45 +0200

    Resume only on unlock

--- app/src/main/java/com/darekbx/lightlauncher/ui/stocks/StockWidget.kt
+++ app/src/main/java/com/darekbx/lightlauncher/ui/stocks/StockWidget.kt
@@ -1,5 +1,7 @@
 package com.darekbx.lightlauncher.ui.stocks
 
+import android.app.KeyguardManager
+import android.content.Context
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -11,9 +13,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,6 +27,7 @@ import com.darekbx.lightlauncher.repository.remote.stocks.StocksProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.koin.compose.koinInject
 
 @Composable
@@ -30,26 +35,47 @@ fun StockWidget(
     type: StocksProvider.StockType,
     stocksProvider: StocksProvider = koinInject()
 ) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+
+    val km = remember { context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager }
+    var wasDeviceLocked by remember { mutableStateOf(km.isDeviceLocked) }
+
     var isLoading by remember { mutableStateOf(false) }
     var previousValue by remember { mutableDoubleStateOf(-1.0) }
     var currentValue by remember { mutableDoubleStateOf(-1.0) }
 
     LifecycleResumeEffect(type) {
-        isLoading = true
-        val job = CoroutineScope(Dispatchers.IO).launch {
-            stocksProvider.fetch(type)?.let {
-                previousValue = currentValue
-                currentValue = it
+        val isLocked = km.isDeviceLocked
+        val shouldFetch = wasDeviceLocked && !isLocked
+
+        val job = scope.launch {
+            if (shouldFetch) {
+                isLoading = true
+
+                val result = withContext(Dispatchers.IO) {
+                    stocksProvider.fetch(type)
+                }
+
+                result?.let {
+                    previousValue = currentValue
+                    currentValue = it
+                }
+
+                isLoading = false
             }
-            isLoading = false
+
+            wasDeviceLocked = isLocked
         }
+
         onPauseOrDispose { job.cancel() }
     }
 
     when {
         isLoading -> Loading()
         currentValue > 0.0 -> StockWidgetText(currentValue, previousValue)
-        else -> { /* Error */ }
+        else -> { /* Error */
+        }
     }
 }
 
